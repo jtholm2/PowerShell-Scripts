020f0cc4b073b52e8613c2d6b19ad8b7eabf5c3f
jtholm2 <56807263+jtholm2@users.noreply.github.com>
2020-04-28 16:46:26 -0700

    Add files via upload

---
+++ text-to-speech.ps1
@@ -0,0 +1,8 @@
+﻿Add-Type -AssemblyName System.speech
+$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
+$location = Read-Host "Please enter the absolute file path in which your text is located"
+$fileName = Read-Host "What is your file name?"
+Set-Location $location
+$fileToRead = Get-Content -Path .\$fileName -Raw
+$speak.Rate = Read-Host "How fast would you like me to read your text? Please enter a number between -10 and 10"
+$speak.Speak($fileToRead)
