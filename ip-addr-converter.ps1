79fc2507d9f4985cfc98264a9960d0d6157ff069
James Holmes <56807263+jtholm2@users.noreply.github.com>
2020-08-14 15:56:57 -0700

    Add files via upload

---
+++ ip-addr-converter.ps1
@@ -0,0 +1,11 @@
+﻿$filePath = Read-Host -Prompt 'Input the absolute file path of the IP addresses you wish to convert'
+$newFileToCreate = Read-Host -Prompt 'Input the absolute file path of the new file in which you wish to store the new IPs'
+
+$ips = Get-Content -Path $filePath
+$subIp = "10."
+for ($i=0; $i -lt $ips.Length; $i++)
+{
+    $ips[$i] = $subIp + $ips[$i].Substring(4)
+    Add-Content -Path $newFileToCreate -Value $ips[$i]
+}
+
